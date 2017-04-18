033addb0babf00d1d7cd1ee8825c6f9f20b16fab
green labs <a@a>
2017-04-18 11:05:54 -0400

    oscope

--- src/wemo_logging.py
+++ src/wemo_logging.py
@@ -65,7 +65,7 @@ class WemoLogger ():
                 self.load_switches()
             time.sleep(self.ASYNCH_RELOAD)
 
-    def load_switches(self, timeout=10):
+    def load_switches(self, timeout=3):
         print "Entering discovery mode for %s seconds" % timeout
         self.env.discover(timeout)
         print "Done with Discovery.  Starting to sync switches"
@@ -89,7 +89,7 @@ class WemoLogger ():
             for i in range(0, 15):
                 if i in self.switches:
                     print i, get_name(i), self.switches[i]
-            raw_input("press enter if naming is correct")
+            #raw_input("press enter if naming is correct")
     
     def get_switch_power(self, i):
         try:
@@ -98,10 +98,8 @@ class WemoLogger ():
         except:
             power = -1
         self.q.put( (i, power))
-
-
-
-    def get_data(self):
+    
+    def get_data_old(self):
         data = [0.0]*self.NUM_DEVICES
         del_list = []
         for i in self.devices:
@@ -114,6 +112,36 @@ class WemoLogger ():
                     print "collecting data from %s took too long " % get_name(i)
                     p.terminate()
                     p.join()
+                    del_list.append(i)
+        for t in range(self.q.qsize()):
+            i, power = self.q.get()
+            data[i] = power
+            if (power == -1) and (i in self.switches) and (i not in del_list):
+                if self.debug: print "removing %s from list because power was 0" % get_name(i)
+                del_list.append(i)
+
+        for i in del_list:
+            del self.switches[i]
+        return data
+
+
+
+    def get_data(self):
+        data = [0.0]*self.NUM_DEVICES
+        del_list = []
+        p = [None]*self.NUM_DEVICES
+        for i in self.devices:
+            if i in self.switches:
+                #TODO Paralleize this
+                p[i] = Process(target=self.get_switch_power, args=(i,))
+                p[i].start()
+        time.sleep(.5)
+        for i in self.devices:
+            if i in self.switches:
+                if p[i] and p[i].is_alive():
+                    print "collecting data from %s took too long " % get_name(i)
+                    p[i].terminate()
+                    #p[i].join()
                     del_list.append(i)
         for t in range(self.q.qsize()):
             i, power = self.q.get()
@@ -161,32 +189,35 @@ class WemoLogger ():
             # add date to list
             data.append ( (date, dat))
 
-if __name__=="__main__":
-    parser = ArgumentParser(usage=\
-        """
-        Welcome to wemo data logging by LEAC-MIT. 
-        Enter the number of devices to log
-        Use argument -d or --debug for printing debug statements
-        
-        eg: python wemo_logging.py 10 
+parser = ArgumentParser(usage=\
+	"""
+	Welcome to wemo data logging by LEAC-MIT. 
+	Enter the number of devices to log
+	Use argument -d or --debug for printing debug statements
 
-        
-    
-        """)
+	eg: python wemo_logging.py 10 
 
-    parser.add_argument('num_devices', type=int)
-    parser.add_argument('-d',"--debug", action="store_true")
-    args = parser.parse_args()
 
-    try:
-        assert args.num_devices > 0 and args.num_devices <= MAX_DEVICES
-    except:
-        print """
-            %s is an invalid number of devices. 
-            The number must be between 1 and %d """ %\
-                    (args.num_devices, MAX_DEVICES)
-        sys.exit(1)
+
+	""")
+parser.add_argument('num_devices', type=int)
+parser.add_argument('-d',"--debug", action="store_true")
+args = parser.parse_args()
+
+try:
+    assert args.num_devices > 0 and args.num_devices <= MAX_DEVICES
+except:
+    print """
+    %s is an invalid number of devices. 
+    The number must be between 1 and %d """ %\
+    (args.num_devices, MAX_DEVICES)
+    sys.exit(1)
+
+def getWL():
     wl = WemoLogger(args.num_devices, args.debug)
+    return wl
 
+if __name__=="__main__":
+    wl = getWL()
     wl.run()
     sys.exit(0)
